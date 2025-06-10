90bce86af0a08707062fed3e792b00790be03c4b
Mauricio Silva <mauricioluan2023@gmail.com>
2025-06-09 22:18:39 -0300

    implementei axios para realizar a pesquisa na api do Google Books.

--- src/components/InputBuscaLivros.vue
+++ src/components/InputBuscaLivros.vue
@@ -3,12 +3,12 @@ Como usuário gostaria de digitar algumas palavras e ver uma lista de
 livros que tenhas relação com a minha busca.
 
 1 - fazer o formulario para pesquisar
-2 - fazer com que cada letra digitada faça uma ação de busca
+2 - testar get na api pra buscar resultado da pesquisa quando clicar no botao
 -->
 <template>
   <form>
     <input v-model="pesquisa" type="text" placeholder="Digite algo: ">
-    <button type="button" @click="resultadoPesquisa">pesquisar</button>
+    <button type="button" @click="requisicao(pesquisa)">pesquisar</button>
   </form>
 </template>
 
@@ -21,14 +21,33 @@ export default {
       pesquisa: '',
     }
   },
+
   props: {},
+
   methods: {
     resultadoPesquisa() {
       console.log(this.pesquisa);
+    },
+    // get para retornar o resultado da pesquisa
+    requisicao(search) {
+      const chamaApi = axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${search}`)
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      return chamaApi;
     }
   },
   computed: {},
-  watch: {},
+
+  watch: {
+    pesquisa() {
+
+    }
+  },
 }
 </script>
 
