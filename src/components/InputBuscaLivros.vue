86b3d8e7ac59c09314334680b6b23b629b15dd40
Mauricio Silva <mauricioluan2023@gmail.com>
2025-06-09 21:38:04 -0300

    Excluídos arquivos padrão e criado component para pesquisa dos livros.

---
+++ src/components/InputBuscaLivros.vue
@@ -0,0 +1,35 @@
+<!--
+Como usuário gostaria de digitar algumas palavras e ver uma lista de
+livros que tenhas relação com a minha busca.
+
+1 - fazer o formulario para pesquisar
+2 - fazer com que cada letra digitada faça uma ação de busca
+-->
+<template>
+  <form>
+    <input v-model="pesquisa" type="text" placeholder="Digite algo: ">
+    <button type="button" @click="resultadoPesquisa">pesquisar</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      pesquisa: '',
+    }
+  },
+  props: {},
+  methods: {
+    resultadoPesquisa() {
+      console.log(this.pesquisa);
+    }
+  },
+  computed: {},
+  watch: {},
+}
+</script>
+
+<style></style>
---
+++ src/views/PgBuscaLivros.vue
@@ -0,0 +1,13 @@
+<template>
+  <InputBuscaLivros />
+</template>
+<script>
+import InputBuscaLivros from '@/components/InputBuscaLivros.vue';
+
+export default {
+  components: {
+    InputBuscaLivros
+  }
+}
+</script>
+<style></style>
